12a566fe6783d34096a23b260005b4306600ac7f
Ikhsan Assaat <ixnixnixn@yahoo.com>
2015-04-18 13:53:44 +0100

    add tests target

---
+++ Demo/CMPopTipViewTests/CMPopTipViewTests.m
@@ -0,0 +1,32 @@
+//
+//  CMPopTipViewTests.m
+//  CMPopTipViewTests
+//
+//  Created by Ikhsan Assaat on 4/18/15.
+//  Copyright (c) 2015 Chris Miles. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <XCTest/XCTest.h>
+#import "CMPopTipView.h"
+
+@interface CMPopTipViewTests : XCTestCase {
+    CMPopTipView *popTipView;
+}
+
+@end
+
+@implementation CMPopTipViewTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    popTipView = nil;
+    [super tearDown];
+}
+
+
+@end
