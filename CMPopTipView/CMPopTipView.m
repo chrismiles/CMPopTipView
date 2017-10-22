e72e395a8e82d641f517dacfe113362e34461fe2
Klein Lieu <kleinvinhlieu@gmail.com>
2017-10-22 16:21:41 -0700

    better initialization of containerView

--- CMPopTipView/CMPopTipView.m
+++ CMPopTipView/CMPopTipView.m
@@ -616,29 +616,15 @@
 
 - (void)presentPointingAtBarButtonItem:(UIBarButtonItem *)barButtonItem animated:(BOOL)animated {
 	UIView *targetView = (UIView *)[barButtonItem performSelector:@selector(view)];
+    UIView *containerView = targetView.window;
     
-    // Try to find the superview of the UINavigationBar. Limit the number of tries to 8.
-    UIView *containerView = targetView.superview;
-    for(NSInteger i = 0; i < 8; ++i) {
-        if ([containerView isKindOfClass:[UIToolbar class]]) {
-            containerView = containerView.superview;
-            break;
-        }
-        
-        if([containerView isKindOfClass:[UINavigationBar class]]) {
-            containerView = containerView.superview;
-            break;
-        }
-        containerView = containerView.superview;
+    if (nil == containerView) {
+        NSLog(@"Cannot determine container view from UIBarButtonItem: %@", barButtonItem);
+        self.targetObject = nil;
+        return;
     }
 
-	if (nil == containerView) {
-		NSLog(@"Cannot determine container view from UIBarButtonItem: %@", barButtonItem);
-		self.targetObject = nil;
-		return;
-	}
-
-	self.targetObject = barButtonItem;
+    self.targetObject = barButtonItem;
 
 	[self presentPointingAtView:targetView inView:containerView animated:animated];
 }
